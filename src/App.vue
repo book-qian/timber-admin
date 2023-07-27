a43861664637a4cc98365e44c5ef1b84ca196c9e
yangyongqian <yangyongqian@whty.com.cn>
2023-07-27 22:38:56 +0800

    完成后台管理系统完成

--- src/App.vue
+++ src/App.vue
@@ -12,4 +12,19 @@
   margin: 0;
   padding: 0;
 }
+
+// 滚动条美化
+::-webkit-scrollbar {
+  width: 5px;
+  height: 5px;
+  position: absolute;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #1890ff;
+}
+
+::-webkit-scrollbar-track {
+  background: #ddd;
+}
 </style>
